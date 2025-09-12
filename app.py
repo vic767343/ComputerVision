1c49cfb6a146ef433bfb4a8334a9fb36c5ca10c8
vic <vic767343@gmail.com>
2025-09-12 23:32:48 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from controller.rootController import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8612)
